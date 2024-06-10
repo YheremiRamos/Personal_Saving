6a8043087e4ac6f009c3b5fa8fc179de0836a821
lesliemarlo <lesliemarlo09@gmail.com>
2024-06-10 17:45:04 -0500

    plantilla_vacia

---
+++ Proyecto.Presentacion/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proyecto.Presentacion.Models;
+using System.Diagnostics;
+
+namespace Proyecto.Presentacion.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult listado()
+        {
+            return View();
+        }
+    }
+}
