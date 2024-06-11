09d039da22092105d6599a851a151497e872ea65
FatimaUrpi <fc200509fatima.chavez09@gmail.com>
2024-06-11 01:35:12 -0500

    avanceEgreso

---
+++ Proyecto.Presentacion/Controllers/EgresoController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Proyecto.Presentacion.Models;
+
+namespace Proyecto.Presentacion.Controllers
+{
+    public class EgresoController : Controller
+    {
+
+
+            //Definir la cadena de conexion 
+            public readonly IConfiguration Configuration;
+
+            public EgresoController(IConfiguration configuration)
+            {
+                this.Configuration = configuration;
+            }
+
+
+            public List<Egreso> aEgreso()
+            {
+                List<Egreso> aEgreso = new List<Egreso>();
+                SqlConnection cn = new SqlConnection(Configuration["ConnectionStrings:cn"]);
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("SP_LISTAR_EGRESO", cn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    aEgreso.Add(new Egreso()
+                    {
+                        id = int.Parse(dr[0].ToString()),
+                        id_usuario = int.Parse(dr[1].ToString()),
+                        id_transaccion = int.Parse(dr[2].ToString()),
+                        fecha = DateTime.Parse(dr[3].ToString()),
+                        monto = double.Parse(dr[4].ToString()),
+                        descripcion = dr[5].ToString()
+
+                    });
+                }
+                cn.Close();
+                return aEgreso;
+            }
+
+            public IActionResult ListaEgreso()
+            {
+                return View(aEgreso());
+            }
+
+            public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Proyecto.Presentacion/Models/Egreso.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace Proyecto.Presentacion.Models
+{
+    public class Egreso
+    {
+
+        [DisplayName("CÓDIGO")]
+
+        public int id { get; set; }
+        [DisplayName("CÓDIGO USUARIO")]
+
+        public int id_usuario { get; set; }
+
+        [DisplayName("CÓDIGO TRANSACCIÓN")]
+
+        public int id_transaccion { get; set; }
+
+        [DisplayName("FECHA")]
+
+        public DateTime fecha { get; set; }
+
+        [DisplayName("MONTO")]
+        public double monto { get; set; }
+
+        [DisplayName("DESCRIPCIÓN")]
+        public string? descripcion { get; set; }
+
+    }
+}
