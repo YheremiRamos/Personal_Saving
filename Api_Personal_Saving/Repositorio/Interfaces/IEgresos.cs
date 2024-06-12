82639c0cedc6ca439551b8b15df84f93f1154405
lesliemarlo <lesliemarlo09@gmail.com>
2024-06-11 22:31:27 -0500

    BACK/listado_de_Ingresos-Activos-Egresos-Listo

---
+++ Api_Personal_Saving/Repositorio/Interfaces/IEgresos.cs
@@ -0,0 +1,20 @@
+﻿using BACK_Api_Personal_Saving.Models;
+
+namespace BACK_Api_Personal_Saving.Repositorio.Interfaces
+{
+    public interface IEgresos
+        //paso2: crear interface (listado)
+    {
+        IEnumerable<Egresos> listarEgresos();
+
+       /*
+        IEnumerable<EgresosO> listadoEgresosO();
+
+        //METODOS
+        EgresosO buscarEgresos(int id);
+        string nuevoEgreso(EgresosO objE);
+
+        //metodo para actualizar pendiente
+        //string modificaPaciente(Paciente objP);*/
+    }
+}
